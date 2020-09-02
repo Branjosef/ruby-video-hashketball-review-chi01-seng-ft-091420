5c25cd2094d30b0f478ccd68d758aabc1a0b6c39
Brandon Fonnemann <branjosef@gmail.com>
2020-09-02 20:13:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,10 +127,4 @@ def game_hash
   }
 end
 
-def players
-  game_hash[:home][:players].merge(game_hash[:away][:players])
-end
 
-def num_points_scored player_name
-  players[player_name][:points]
-end 
